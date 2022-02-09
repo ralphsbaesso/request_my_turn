ec3f391d49e2afb67180cb96c46cbc572e0a7ad8
ralph baesso <WO7Q/xWOa7wTplkZj6l8Gx6j9jqDYsqQX5qXevyHsKE=>
2022-02-09 14:24:04 -0300

    init

---
+++ lib/request_my_turn.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'request_my_turn/version'
+
+class RequestMyTurn
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/request_my_turn/turn_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe RequestMyTurn do
+  it 'has a version number' do
+    expect(RequestMyTurn::VERSION).not_to be nil
+  end
+end
